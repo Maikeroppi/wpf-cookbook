5b0fb8c88e558c3912dc7dc06fff209d2403983d
Michael Eads <maikeroppi@gmail.com>
2017-08-19 11:15:43 -0700

    CH01 CustomAttached

---
+++ CH01.CustomAttached/CH01.CustomAttached/RotationManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CH01.CustomAttached
+{
+    class RotationManager
+    {
+        public static double GetAngle(DependencyObject obj)
+        {
+            return (double)obj.GetValue(AngleProperty);
+        }
+
+        public static void SetAngle(DependencyObject obj, double value)
+        {
+            obj.SetValue(AngleProperty, value);
+        }
+
+        private static void OnAngleChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var element = obj as UIElement;
+            if (element != null)
+            {
+                element.RenderTransformOrigin = new Point(0.5, 0.5);
+                element.RenderTransform = new RotateTransform((double)args.NewValue);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.RegisterAttached("Angle", typeof(double), 
+                typeof(RotationManager), new UIPropertyMetadata(0.0, OnAngleChanged));
+
+
+    }
+}
