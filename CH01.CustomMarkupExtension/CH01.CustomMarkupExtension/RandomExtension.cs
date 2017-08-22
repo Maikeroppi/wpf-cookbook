921ea33cf3e3021472b4a6d9117365120a858137
Michael Eads <maikeroppi@gmail.com>
2017-08-22 08:40:02 -0700

    CH01 CustomMarkupExtension
    Fix issue with including RandomExtension and make ProvideValue more generic

--- CH01.CustomMarkupExtension/CH01.CustomMarkupExtension/RandomExtension.cs
+++ CH01.CustomMarkupExtension/CH01.CustomMarkupExtension/RandomExtension.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Markup;
 
 namespace CH01.CustomMarkupExtension
@@ -10,18 +12,46 @@ namespace CH01.CustomMarkupExtension
     public class RandomExtension : MarkupExtension
     {
         readonly int _from, _to;
-        static readonly Random _rnd = new Random();
         public RandomExtension(int from, int to)
         {
             _from = from; _to = to;
         }
+
         public RandomExtension(int to) : this(0, to)
         {
-        }
 
+        }
+        static readonly Random _rnd = new Random();
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            return (double)_rnd.Next(_from, _to);
+            int value = _rnd.Next(_from, _to);
+            Type targetType = null;
+
+            if (serviceProvider != null)
+            {
+                var target = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
+                if (target != null)
+                {
+                    var clrProp = target.TargetProperty as PropertyInfo;
+                    if (clrProp != null)
+                    {
+                        targetType = clrProp.PropertyType;
+                    }
+
+                    if (targetType == null)
+                    {
+                        var dp = target.TargetProperty as DependencyProperty;
+                        if (dp != null)
+                        {
+                            targetType = dp.PropertyType;
+                        }
+                    }
+                }
+            }
+
+            return targetType != null
+                       ? Convert.ChangeType(value, targetType)
+                       : Convert.ToString(value);
         }
     }
 }
